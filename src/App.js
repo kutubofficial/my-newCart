6e4e63b74229cc27d214a388d8833569707c1a20
Kutub <kutubofficial599@gmail.com>
2023-04-02 02:02:17 +0530

    checking to new update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import './App.css';
 import Employees from './Components/Employees';
 function App() {
   return (
-    <div>
-     <Employees name="Kutubuddin" address="New delhi" category="Engineer"/>
+    <div className='App'>
+     <Employees name="Sourabh Chaudhary" address="Gurgaon" category="Electrical" contact="+917268866332"/>
+     <Employees name="Rohit Yadav" address="Uttar Pradesh" category="Civil" contact="+91824567897"/>
+     <Employees name="Sumit Chauhan" address="Haridwar" category="Machenical" contact="+9187576498"/>
+     <Employees name="Prashant Rawat" address="Noida" category="Fiter" contact="+916098986574"/>
+     <Employees name="Akhilesh Kumar" address="Mumbai" category="Helper" contact="+917897786798"/>
     </div>
   );
 }
--- src/Components/Employees.js
+++ src/Components/Employees.js
@@ -1,9 +1,18 @@
+import './Employees.css'
 const Employees = (props)=>{
     return(
-        <div>
-            <h1>My Name: {props.name}</h1>
+        <div className="container">
+            <div className='box'>
+               Employee I'd: <input type='text' id='id'/>
+               Password: <input type='text' id='password'/>
+               <button id='btn'>Done</button>
+               </div>
+            <div className='box'>
+            <h1>Employee Name: {props.name}</h1>
             <h1>Address: {props.address}</h1>
+            <h1>Contact: {props.contact}</h1>
             <h1>Category: {props.category}</h1>
+            </div>
         </div>
     )
 }
